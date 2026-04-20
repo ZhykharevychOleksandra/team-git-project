75473debd3e455006f1f11c4d572f7b8d990fc34
OksanaSydor <sydor.oksana1111@gmail.com>
2026-04-20 22:58:40 +0300

    Add add() and subtract() methods

--- main.pppi/Calculator.cpp
+++ main.pppi/Calculator.cpp
@@ -3,14 +3,24 @@
 
 int Calculator::multiply(int a, int b)
 {
-    return a * b;
+  return a * b;
 }
 
 int Calculator::divide(int a, int b)
 {
-    if (b == 0)
-    {
-        throw std::runtime_error("Division by zero");
-    }
-    return a / b;
+  if (b == 0)
+  {
+    throw std::runtime_error("Division by zero");
+  }
+  return a / b;
+}
+
+int Calculator::add(int a, int b)
+{
+  return a + b;
+}
+
+int Calculator::subtract(int a, int b)
+{
+  return a - b;
 }
