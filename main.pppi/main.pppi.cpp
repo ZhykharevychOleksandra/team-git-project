42a119f5f83ef00c60f4ba6c65c13e9f27f2de69
Олександра Жихаревич <oleksandraziharevic@gmail.com>
2026-04-20 23:45:01 +0300

    Changed add() logic by Zhykharevych

---
+++ main.pppi/main.pppi.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main() {
+    Calculator calc;
+
+    int a, b;
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Sum: " << calc.add(a, b) << endl;
+    cout << "Subtract: " << calc.subtract(a, b) << endl;
+    cout << "Multiply: " << calc.multiply(a, b) << endl;
+    cout << "Divide: " << calc.divide(a, b) << endl;
+
+    return 0;
+}
