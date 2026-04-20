32fd4bad4c42d99a2b261504bf342dfa36cc9345
unknown <karinaglusenko967@gmail.com>
2026-04-20 18:27:12 +0300

    Add multiply() and divide() methods

--- main.pppi/Calculator.cpp
+++ main.pppi/Calculator.cpp
@@ -1,1 +1,15 @@
-#include "Calculator.h"
+#include <stdexcept>
+
+int Calculator::multiply(int a, int b)
+{
+    return a * b;
+}
+
+int Calculator::divide(int a, int b)
+{
+    if (b == 0)
+    {
+        throw std::runtime_error("Division by zero");
+    }
+    return a / b;
+}
