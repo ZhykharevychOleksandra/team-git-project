ae9e85ce2d9f2ce3f226936676d647be5721ffe8
Олександра Жихаревич <oleksandraziharevic@gmail.com>
2026-04-21 00:03:52 +0300

    Changed add() logic by Zhykharevych

--- main.pppi/Calculator.cpp
+++ main.pppi/Calculator.cpp
@@ -15,9 +15,8 @@ int Calculator::divide(int a, int b)
   return a / b;
 }
 
-int Calculator::add(int a, int b)
-{
-  return a + b;
+int Calculator::add(int a, int b) {
+    return a + b + 2; // change by Zhykharevych
 }
 
 int Calculator::subtract(int a, int b)
